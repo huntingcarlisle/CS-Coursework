ca987c474c1a29c3a05c3736c76fdaf21eda4f12
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-06-03 11:21:29 -0700

    UBC data abstraction

---
+++ UBC/3_dataAbstraction/Coin/src/model/Coin.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Coin {
+
+    // REQUIRES: nothing
+    // MODIFIES: this
+    // EFFECTS: generates a random value to set coin value to 0 or 1
+    public void flip() {
+
+    };
+
+    // REQUIRES: nothing
+    // MODIFIES: nothing
+    // EFFECTS: returns "heads" if coin value is 0, "tails" if coin value is 1
+    public String checkStatus() {
+        return "heads";
+    };
+
+
+}
---
+++ UBC/3_dataAbstraction/Coin/src/ui/Main.java
@@ -0,0 +1,12 @@
+package ui;
+
+import model.Coin;
+
+public class Main {
+    public void main(String args[]) {
+        Coin coin = new Coin();
+        coin.flip();
+        System.out.print(coin.checkStatus());
+
+    }
+}
