82b3bdd44fdfea08892ab529d0792798c727fbb9
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-01 11:35:06 -0700

    assn 5 complete

---
+++ 00_College Courses/Foothill College/C++ Programming/Assn 5/cmake-build-debug/CMakeFiles/Assn_5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assn_5.dir/main.cpp.obj"
+  "Assn_5.pdb"
+  "Assn_5.exe"
+  "Assn_5.exe.manifest"
+  "Assn_5.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assn_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
