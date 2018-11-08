31101e1745faeef3a8c4c92cb8680d70dec61e5a
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-07 21:55:16 -0800

    assn 6 setup

---
+++ 00_College Courses/Foothill College/C++ Programming/Assignment 06/cmake-build-debug/CMakeFiles/Assignment_06.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment_06.dir/main.cpp.obj"
+  "Assignment_06.pdb"
+  "Assignment_06.exe"
+  "Assignment_06.exe.manifest"
+  "Assignment_06.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment_06.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
