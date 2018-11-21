de5d63500a02aa13c25488113dfddccebcad453f
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-20 21:57:16 -0800

    assn 07 complete

--- 00_College Courses/Foothill College/Python for Programmers 2/Assignment 07/HunterCarlisleLab7client.py
+++ 00_College Courses/Foothill College/Python for Programmers 2/Assignment 07/HunterCarlisleLab7client.py
@@ -1,6 +1,34 @@
 """
 Hunter Carlisle | Foothill College Fall 2018 | Lab Seven
 
-This program converts distances in kilometers to miles.
+This is the client for a simple message echo program.
 
+"""
+import socket
+
+# Initialize Variables
+HOST = socket.gethostname()
+PORT = 55555
+MESSAGE = "Demo Message - client: Hunter Carlisle"
+BUFFER_SIZE = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.connect((HOST, PORT))
+except socket.error as e:
+    print(str(e))
+
+s.send(str.encode(MESSAGE))
+
+data = s.recv(BUFFER_SIZE)
+
+s.close()
+print('Received', repr(data))
+
+
+""" PROGRAM RUN: CLIENT
+Received b'Demo Message - client: Hunter Carlisle'
+
+Process finished with exit code 0
 """
--- 00_College Courses/Foothill College/Python for Programmers 2/Assignment 07/HunterCarlisleLab7server.py
+++ 00_College Courses/Foothill College/Python for Programmers 2/Assignment 07/HunterCarlisleLab7server.py
@@ -1,6 +1,42 @@
 """
 Hunter Carlisle | Foothill College Fall 2018 | Lab Seven
 
-This program converts distances in kilometers to miles.
+This is the server for a simple message echo program.
 
+"""
+import socket
+
+HOST = socket.gethostname()
+PORT = 55555
+BUFFER_SIZE = 1024
+LISTEN_BACKLOG = 5
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((HOST, PORT))
+except socket.error as e:
+    print(str(e))
+
+s.listen(LISTEN_BACKLOG)
+print("Waiting for Connection...")
+
+conn, addr = s.accept()
+print('Connected by', addr)
+
+while True:
+    data = conn.recv(BUFFER_SIZE)
+    if not data:
+        break
+    conn.send(data)
+
+conn.close()
+s.close()
+
+
+""" PROGRAM RUN: SERVER
+Waiting for Connection...
+Connected by ('192.168.1.4', 62078)
+
+Process finished with exit code 0
 """
