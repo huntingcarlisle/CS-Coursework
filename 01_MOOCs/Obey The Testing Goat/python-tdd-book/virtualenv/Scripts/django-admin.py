8beff5ee935ea64b25e16c2bd65eea22961598a9
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-01-21 21:08:06 -0800

    Basic Django config

---
+++ 01_MOOCs/Obey The Testing Goat/python-tdd-book/virtualenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hucar\dropbox\code\cs-coursework\01_moocs\obey the testing goat\python-tdd-book\virtualenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
