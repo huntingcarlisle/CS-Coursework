05b0f24262e077cdfe291fb64c52b524bfdf5285
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-01-25 12:08:54 -0800

    Post model created and migrated

---
+++ 03_Self Directed/Django_Girls/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\hucar\Dropbox\Code\CS-Coursework\03_Self Directed\Django_Girls\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
